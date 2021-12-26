6567f7e40dfdf090897aa8571ef578999be45372
unknown <igatatsu1997@gmail.com>
2021-12-27 08:24:59 +0900

    Fixed pom.xml

--- src/main/java/servlet/TopServlet.java
+++ src/main/java/servlet/TopServlet.java
@@ -7,13 +7,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
 public class TopServlet extends HttpServlet {
 	
-	/**
-	 * 
-	 */
+	
+	
 	private static final long serialVersionUID = 1L;
 
+	@RequestMapping("/")
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 		request.setCharacterEncoding("utf-8");
 		
